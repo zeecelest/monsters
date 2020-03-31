95e120c16ac50616bc2147a19e64e51ba6208ecd
Zee Celest <zeecelest@gmail.com>
2020-03-31 17:31:17 -0400

    fetch call successful

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
+     
     this.state = {
       monsters: []
     };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users'),
-    .then('')
-    .then('')
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users }));
   }
 
   render() {

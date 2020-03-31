f92ecd246a3912574bdb8ac7be93d5d4a12780de
Zee Celest <zeecelest@gmail.com>
2020-03-31 17:26:54 -0400

    creating componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: 'Hello'
-    }
+      monsters: []
+    };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users'),
+    .then('')
+    .then('')
+  }
+
   render() {
     return(
       <div className='App'>
-        <header className='App-header'>
-            <img src={logo} className='App-logo'/>
-            <p>{this.state.string}</p>
-            <button onClick={() => this.setState({ string: 'Sup'})}>Change Text</button>
-        </header>
+        {
+          this.state.monsters.map(monster => 
+          <h1 key={monster.id}> {monster.name}</h1>)
+
+        }
+    
       </div>
-      
     );
-
   }
+}
  
-
-
 export default App;
